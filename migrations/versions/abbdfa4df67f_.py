f38c6253295cae920402c9924008f01cd67d75ea
dvjones27 <102387309+dvjones27@users.noreply.github.com>
2023-05-04 05:59:37 -0400

    Modified .env file updating DB

--- migrations/versions/abbdfa4df67f_.py
+++ migrations/versions/abbdfa4df67f_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9693d187ebde
+Revision ID: abbdfa4df67f
 Revises: 
-Create Date: 2023-05-04 05:30:03.557233
+Create Date: 2023-05-04 05:59:01.185302
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9693d187ebde'
+revision = 'abbdfa4df67f'
 down_revision = None
 branch_labels = None
 depends_on = None
